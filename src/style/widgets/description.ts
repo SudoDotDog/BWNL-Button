e4d5562ed5e42d2a24ea5b5f3acf6d591f9cb4c7
WMXPY <wm@mengw.io>
2019-12-10 12:57:30 -0600

    implement description

--- src/style/widgets/description.ts
+++ src/style/widgets/description.ts
@@ -8,6 +8,30 @@ import { JSSStyle, StyleManager } from "@sudoo/jss";
 
 const DescriptionWidgetStyleBase: JSSStyle = {
 
+    title: {
+        fontWeight: 'bold',
+        fontSize: '20px',
+        paddingLeft: '7px',
+    },
+    descriptions: {
+        padding: '5px',
+        paddingLeft: '14px',
+        display: 'grid',
+        rowGap: '5px',
+    },
+    complex: {
+        display: 'flex',
+        alignItems: 'center',
+    },
+    complexIcon: {
+        display: 'inherit',
+        margin: 0,
+        padding: 0,
+    },
+    complexText: {
+        paddingLeft: '5px',
+        flex: 1,
+    },
 };
 
 export const DescriptionWidgetStyle: StyleManager = StyleManager.create(DescriptionWidgetStyleBase, 'widget-description').setPrefix('BWNL-Button-');
--- src/widgets/descriptions.tsx
+++ src/widgets/descriptions.tsx
@@ -24,10 +24,50 @@ export class DescriptionWidget extends React.PureComponent<DescriptionWidgetProp
 
     private readonly _descriptionStyle: Classes = DescriptionWidgetStyle.use();
 
+    public constructor(props: DescriptionWidgetProps) {
+
+        super(props);
+
+        this._renderDescription = this._renderDescription.bind(this);
+    }
+
     public render() {
 
-        return (<div>
-            {this.props.title}
+        return (<div className={this._descriptionStyle.wrapper}>
+            <div className={this._descriptionStyle.title}>
+                {this.props.title}
+            </div>
+            <div className={this._descriptionStyle.descriptions}>
+                {this._renderDescriptions()}
+            </div>
+        </div>);
+    }
+
+    private _renderDescriptions() {
+
+        const descriptions: DescriptionElement[] = this.props.descriptions;
+        if (!Array.isArray(descriptions)) {
+            return null;
+        }
+
+        return descriptions.map(this._renderDescription);
+    }
+
+    private _renderDescription(description: DescriptionElement, index: number) {
+
+        if (description.icon) {
+
+            return (<div
+                key={index}
+                className={this._descriptionStyle.complex}
+            >
+                <div className={this._descriptionStyle.complexIcon}>{description.icon}</div>
+                <div className={this._descriptionStyle.complexText}>{description.text}</div>
+            </div>);
+        }
+
+        return (<div key={index}>
+            {description.text}
         </div>);
     }
 }
