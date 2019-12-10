8a65ef8dc107f845ba53df33695d1583cfe3dec8
WMXPY <wm@mengw.io>
2019-12-10 16:17:33 -0600

    init holdable button

--- src/hold.tsx
+++ src/hold.tsx
@@ -4,10 +4,16 @@
  * @description Hold
  */
 
+import { Classes } from "jss";
 import * as React from "react";
+import { HoldStyle } from "./style/hold";
 
 export type HoldableButtonProps = {
 
+    readonly wrapperStyle?: React.CSSProperties;
+    readonly width?: string;
+    readonly block?: boolean;
+    readonly borderColor?: string;
 };
 
 export type HoldableButtonStates = {
@@ -21,16 +27,28 @@ export class HoldableButton extends React.Component<HoldableButtonProps, Holdabl
 
     };
 
+    private readonly _holdStyle: Classes = HoldStyle.use();
+
     public render() {
 
-        return React.createElement('div',
-            {
-
-            },
-            this.props.children,
-            React.createElement('div', {
-
-            }, 'hold'),
-        );
+        return (<div style={{
+            width: this.props.width,
+        }}>
+            <div
+                style={{
+                    ...this.props.wrapperStyle,
+                    borderColor: this.props.borderColor,
+                }}
+                className={this._holdStyle.wrapper}
+            >
+                <div className={this._holdStyle.content}>
+                    <button
+                        className={this._holdStyle.button}
+                    >
+                        {this.props.children}
+                    </button>
+                </div>
+            </div>
+        </div>);
     }
 }
--- src/style/hold.ts
+++ src/style/hold.ts
@@ -6,7 +6,36 @@
 
 import { JSSStyle, StyleManager } from "@sudoo/jss";
 
-const holdStyleBase: JSSStyle = {
+const HoldStyleBase: JSSStyle = {
+
+    wrapper: {
+        borderWidth: '3px',
+        borderStyle: 'solid',
+        borderColor: 'white',
+        position: 'relative',
+        boxSizing: 'border-box',
+        width: '100%',
+        paddingBottom: '100%',
+    },
+    content: {
+        position: "absolute",
+        top: 0,
+        left: 0,
+        width: "100%",
+        height: "100%",
+    },
+    button: {
+        cursor: 'pointer',
+        borderRadius: 0,
+        border: 0,
+        height: '100%',
+        width: '100%',
+        outline: 0,
+        fontSize: '16px',
+        '&:hover': {
+
+        },
+    },
 };
 
-export const holdStyle: StyleManager = StyleManager.create(holdStyleBase, 'hold').setPrefix('BWNL-Button-');
+export const HoldStyle: StyleManager = StyleManager.create(HoldStyleBase, 'hold').setPrefix('BWNL-Button-');
