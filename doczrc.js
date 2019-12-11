a1dbb9511b6ce24eec3ed501464f2e5f3e326701
WMXPY <wm@mengw.io>
2019-12-11 13:34:23 -0600

    init button clicking

--- doczrc.js
+++ doczrc.js
@@ -6,6 +6,8 @@
 
 export default {
     title: 'BWNL Button',
+    filterComponents: (files) =>
+        files.filter(filePath => /[w-]*.(js|jsx|ts|tsx)$/.test(filePath)),
     codeSandbox: false,
     typescript: true,
 }
--- src/components/button.tsx
+++ src/components/button.tsx
@@ -6,7 +6,7 @@
 
 import { Classes } from "jss";
 import * as React from "react";
-import { CoreButtonStyle } from "../style/button";
+import { CoreButtonStyle } from "../style/components/button";
 import { Square } from "./square";
 
 export type CoreButtonProps = {
@@ -15,6 +15,8 @@ export type CoreButtonProps = {
     readonly width?: string;
     readonly block?: boolean;
     readonly borderColor?: string;
+
+    readonly onClick?: () => void;
 };
 
 export type CoreButtonStates = {
@@ -29,6 +31,20 @@ export class CoreButton extends React.Component<CoreButtonProps, CoreButtonState
     };
 
     private readonly _coreButtonStyle: Classes = CoreButtonStyle.use();
+    private _holdTimer: any;
+
+    public constructor(props: CoreButtonProps) {
+
+        super(props);
+
+        this._handleMouseIn = this._handleMouseIn.bind(this);
+        this._handleMouseOut = this._handleMouseOut.bind(this);
+
+        this._handleMouseDown = this._handleMouseDown.bind(this);
+        this._handleMouseUp = this._handleMouseUp.bind(this);
+        this._handleKeyDown = this._handleKeyDown.bind(this);
+        this._handleKeyUp = this._handleKeyUp.bind(this);
+    }
 
     public render() {
 
@@ -42,9 +58,46 @@ export class CoreButton extends React.Component<CoreButtonProps, CoreButtonState
         >
             <button
                 className={this._coreButtonStyle.button}
+                onContextMenu={(e) => e.preventDefault()}
+                onMouseEnter={this._handleMouseIn}
+                onMouseLeave={this._handleMouseOut}
             >
                 {this.props.children}
             </button>
         </Square>);
+    }
+
+    private _handleMouseIn() {
+
+        window.addEventListener('mousedown', this._handleMouseDown);
+        window.removeEventListener('mouseup', this._handleMouseUp);
+    }
+
+    private _handleMouseOut() {
+
+        window.removeEventListener('mousedown', this._handleMouseDown);
+        window.removeEventListener('mouseup', this._handleMouseUp);
+    }
+
+    private _handleMouseDown(e: MouseEvent) {
+
+        e.stopPropagation();
+        e.preventDefault();
+        console.log(e);
+    }
+
+    private _handleMouseUp(e: MouseEvent) {
+
+        console.log(e);
+    }
+
+    private _handleKeyDown() {
+
+
+    }
+
+    private _handleKeyUp() {
+
+
     }
 }
